2de32c4979b51224d8694a1a0b6bf2df8f8d76fa
mikaelcaua <mikaelcauasilva1@gmail.com>
2025-05-01 10:18:37 -0300

    initializing project

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+    >
+      <Text className="font-bold">Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
